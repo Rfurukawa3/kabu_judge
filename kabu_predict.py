30e056bbae484cbeacaf58278f695cddd45e07b5
Furukawa Ryota <rfurukawa3@gmail.com>
2020-04-16 18:30:37 +0900

    変動型を判定する関数をクラスとしてまとめた

--- kabu_predict.py
+++ kabu_predict.py
@@ -2,36 +2,9 @@ import sys
 import os
 import csv
 
-
-def judge_fluctuating():
-    pass
-
-# 減少型がありえるかを判定する、base=買値、price=売値、price0=1個前の売値、magnification_acc=累積した補正倍率
-def judge_decreasing(base, price, price0=None, magnification_acc=[0.0, 0.0]):
-    # magnification_acc[0]=ありえる最低の累積倍率、magnification_acc[1]=ありえる最高の累積倍率
-    min_price = base * (0.85 - 0.03 - 0.02 + magnification_acc[0])
-    max_price = base * (0.90 - 0.03 + magnification_acc[1])
-    if((price >= min_price) and (price <= max_price)):
-        judge = True
-    else:
-        judge = False
-
-    if((magnification_acc == [0.0, 0.0]) or (price0 is None)):
-        magnification_acc = [-0.05, -0.03]
-    else:
-        magnification_acc += (price - price0) / base
-    
-    return judge, magnification_acc
-
-def judge_smallspike():
-    pass
-
-def judge_largespike():
-    pass
-
-
 days = ('月曜AM','月曜PM','火曜AM','火曜PM','水曜AM','水曜PM','木曜AM','木曜PM','金曜AM','金曜PM','土曜AM','土曜PM')
 
+# カブ価関連情報を保持するクラス
 class kabu_log:
     def __init__(self, data):
         if(tuple(data.keys()) != ('名前', '先週', '日曜', '月曜AM','月曜PM','火曜AM','火曜PM','水曜AM','水曜PM','木曜AM','木曜PM','金曜AM','金曜PM','土曜AM','土曜PM')):
@@ -39,15 +12,15 @@ class kabu_log:
 
         self.username = data['名前']
         self.base = int(data['日曜'])
-
         self.prices = dict()
+        self.types = dict()
+
         for day in days:
             if(data[day] == ''):
                 self.prices[day] = 0
             else:
                 self.prices[day] = int(data[day])
 
-        self.types = dict()
         if(data['先週'] == "波型"):
             self.types = {"波型" : 0.20, "減少型" : 0.15, "跳ね小型" : 0.35, "跳ね大型" : 0.30}
         elif(data['先週'] == "減少型"):
@@ -59,19 +32,53 @@ class kabu_log:
         else:
             self.types = {"波型" : 0.25, "減少型" : 0.25, "跳ね小型" : 0.25, "跳ね大型" : 0.25}
 
-
+# kabu_logに変動型判定機能を追加したクラス
+class kabu_judge(kabu_log):
+    def __init__(self,data):
+        super().__init__(data)
+        self.pre_price = None # 1個前の売値
+        self.magnification_acc = [0.0, 0.0] # 累積した補正倍率、[0]=最低値、[1]=最高値
+        self.stage = None # 変動の段階
     
-    def judge(self):
-        magnification_acc = [0.0, 0.0]
-        price0 = None
-        judge = True
+    def reset(self):
+        self.pre_price = None
+        self.magnification_acc = [0.0, 0.0]
+        self.stage = None
+
+    # 波型
+    def fluctuating(self):
+        pass    
+
+    # 減少型
+    def decreasing(self):
+        self.reset()
         for day in days:
             if(self.prices[day] == 0): break
-            judge, magnification_acc = judge_decreasing(self.base, self.prices[day], price0, magnification_acc)
-            price0 = self.prices[day]
-            if(not judge):
+            min_price = self.base * (0.85 - 0.03 - 0.02 + self.magnification_acc[0])
+            max_price = self.base * (0.90 - 0.03 + self.magnification_acc[1])
+            if((self.prices[day] < min_price) or (self.prices[day] > max_price)):
                 self.types.pop("減少型")
                 break
+
+            if((self.magnification_acc == [0.0, 0.0]) or (self.pre_price is None)):
+                self.magnification_acc = [-0.05, -0.03]
+            else:
+                self.magnification_acc += (self.prices[day] - self.pre_price) / self.base
+            self.pre_price = self.prices[day]
+
+    # 跳ね小型
+    def smallspike(self):
+        pass
+
+    # 跳ね大型
+    def largespike(self):
+        pass
+    
+    def judge(self):
+        self.fluctuating()
+        self.decreasing()
+        self.smallspike()
+        self.largespike()
 
         types_sum = 0.0
         for p in self.types.values(): types_sum += p
@@ -81,7 +88,9 @@ class kabu_log:
             self.types[key] /= types_sum
             p = self.types[key]*100
             print(key + f" = {p:.1f}%, ", end='')
-        print('')
+        print('')        
+
+
 
 
 if __name__ == '__main__':
@@ -97,7 +106,7 @@ if __name__ == '__main__':
     log = list()
     with open(args[1], 'r') as f:
         for row in csv.DictReader(f):
-            log.append(kabu_log(row))
+            log.append(kabu_judge(row))
 
     for user in log:
         user.judge()
